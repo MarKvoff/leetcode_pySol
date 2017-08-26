b33439085cc8bcbf51efe0da50a5a4fee338f3fe
MarKvoff <Data@sjtu.edu.cn>
2017-08-26 17:48:12 +0800

    Add files via upload

---
+++ solutions.py
@@ -0,0 +1,15 @@
+'''
+This is the solutions for leetcode in python version
+
+'''
+
+
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+
